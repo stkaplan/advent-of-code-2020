a1694562262f6d041019671f0896c0e398a1b325
Sam Kaplan <stkaplan@gmail.com>
2020-12-02 12:13:29 -0500

    Day 2 part 1

---
+++ 02/02.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import re
+import unittest
+from collections import namedtuple
+from io import StringIO
+
+Policy = namedtuple('Policy', ['min', 'max', 'letter'])
+
+def parse_line(line):
+    line = line.rstrip()
+    match = re.fullmatch(r'([0-9]+)-([0-9]+) ([a-zA-Z]): (.+)', line)
+    try:
+        groups = match.groups()
+        return Policy(int(groups[0]), int(groups[1]), groups[2]), groups[3]
+    except:
+        raise Exception(f'invalid input line: "{line}"')
+
+def read_input(f):
+    return list(map(parse_line, f))
+
+def password_is_valid(policy, password):
+    return policy.min <= password.count(policy.letter) <= policy.max
+
+class Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.input = StringIO('1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc\n')
+
+    def test_read_input(self):
+        input_ = read_input(self.input)
+        self.assertEqual(input_, [((1, 3, 'a'), 'abcde'), ((1, 3, 'b'), 'cdefg'), ((2, 9, 'c'), 'ccccccccc')])
+        valid = [password_is_valid(policy, password) for policy, password in input_]
+        self.assertEqual(valid, [True, False, True])
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+
+    with open('input.txt') as f:
+        input_ = read_input(f)
+
+    valid = [password_is_valid(policy, password) for policy, password in input_]
+    print(sum(valid))
