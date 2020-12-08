364372fc94de077601104e60c05f65a9de7197fe
Sam Kaplan <stkaplan@gmail.com>
2020-12-08 16:30:18 -0500

    Day 8 part 2

--- 08/08.py
+++ 08/08.py
@@ -30,19 +30,48 @@ class Program:
         else:
             raise Exception(f'invalid opcode: "{instruction.opcode}"')
 
-    def run_until_duplicate(self):
+    def check_for_infinite_loop(self):
         instructions_seen = set()
-        while self.pc not in instructions_seen:
+        while True:
+            if self.pc in instructions_seen:
+                return True
+            elif self.pc == len(self.code):
+                return False
             instructions_seen.add(self.pc)
             self.run_next_instruction()
 
+def find_corrupted_instruction(code_orig):
+    for i in range(len(code_orig)):
+        if code_orig[i].opcode not in ['jmp', 'nop']:
+            continue
+
+        code = code_orig.copy()
+        if code[i].opcode == 'jmp':
+            code[i] = Instruction('nop', code[i].argument)
+        elif code[i].opcode == 'nop':
+            code[i] = Instruction('jmp', code[i].argument)
+
+        program = Program(code)
+        infinite = program.check_for_infinite_loop()
+        if not infinite:
+            return (i, program.accumulator)
+
+    return None
+
 class Test(unittest.TestCase):
-    def test_run_until_duplicate(self):
+    def test_check_for_infinite_loop(self):
         with open('test1.txt') as f:
             code = parse_input(f)
         program = Program(code)
-        program.run_until_duplicate()
+        infinite = program.check_for_infinite_loop()
+        self.assertEqual(infinite, True)
         self.assertEqual(program.accumulator, 5)
+
+    def test_find_corrupted_instruction(self):
+        with open('test1.txt') as f:
+            code = parse_input(f)
+        corrupted = find_corrupted_instruction(code)
+        self.assertEqual(corrupted, (7, 8))
 
 if __name__ == '__main__':
     unittest.main(exit=False)
@@ -50,5 +79,5 @@ if __name__ == '__main__':
     with open('input.txt') as f:
         code = parse_input(f)
     program = Program(code)
-    program.run_until_duplicate()
-    print(program.accumulator)
+    corrupted = find_corrupted_instruction(code)
+    print(corrupted)
