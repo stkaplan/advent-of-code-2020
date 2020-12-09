daf63cbf9fc6a2579e31bd6317d203a69a4312bc
Sam Kaplan <stkaplan@gmail.com>
2020-12-09 13:02:52 -0500

    Day 9 part 2

--- 09/09.py
+++ 09/09.py
@@ -23,6 +23,16 @@ def find_invalid_sum(preamble, sums):
         preamble.popleft()
         preamble.append(n)
 
+def find_contiguous_sum(ns, target):
+    for start in range(len(ns)):
+        for end in range(start+1, len(ns)):
+            slice_ = ns[start:end+1]
+            sum_ = sum(slice_)
+            if sum_ == target:
+                return slice_ # Found it.
+            elif sum_ > target:
+                break # Sum is too large, no need to continue with this starting index.
+
 class Test(unittest.TestCase):
     def test_is_sum(self):
         with open('test1.txt') as f:
@@ -37,9 +47,19 @@ class Test(unittest.TestCase):
             preamble, sums = parse_input(f, 5)
         self.assertEqual(find_invalid_sum(preamble, sums), 127)
 
+    def test_find_contiguous_sum(self):
+        with open('test1.txt') as f:
+            _, sums = parse_input(f, 0)
+        sum_range = find_contiguous_sum(sums, 127)
+        self.assertEqual(min(sum_range), 15)
+        self.assertEqual(max(sum_range), 47)
+
 if __name__ == '__main__':
     unittest.main(exit=False)
 
     with open('input.txt') as f:
         preamble, sums = parse_input(f, 25)
-    print(find_invalid_sum(preamble, sums))
+    invalid_sum = find_invalid_sum(preamble, sums)
+    ns = list(preamble) + sums
+    slice_ = find_contiguous_sum(ns, invalid_sum)
+    print(min(slice_) + max(slice_))
