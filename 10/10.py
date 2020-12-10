390b40a50e8fc8a4e828cca06ac79a248bbb2ef2
Sam Kaplan <stkaplan@gmail.com>
2020-12-10 13:54:36 -0500

    Day 10 part 2: slow solution, too slow to complete

--- 10/10.py
+++ 10/10.py
@@ -14,7 +14,7 @@ def find_next_adapter(adapters, joltage):
         if joltage + difference in adapters:
             return joltage + difference
 
-def get_adapter_chain(adapters):
+def get_complete_adapter_chain(adapters):
     chain = [0]
     joltage = 0
     while len(adapters) > 0:
@@ -25,16 +25,37 @@ def get_adapter_chain(adapters):
     return chain
 
 def get_difference_distribution(adapters):
-    chain = get_adapter_chain(adapters)
+    chain = get_complete_adapter_chain(adapters)
     diffs = list(map(operator.sub, chain[1:], chain))
     return Counter(diffs)
 
+def find_possible_next_adapters(adapters, joltage):
+    return [next_joltage for next_joltage in range (joltage+1, joltage+max_joltage_difference+1) if next_joltage in adapters]
+
+def get_all_adapter_chains(adapters):
+    completed_chains = []
+    chains = [[0]]
+    max_joltage = max(adapters)
+    print('max = ', max_joltage)
+    while chains:
+        new_chains = []
+        for chain in chains:
+            for n in find_possible_next_adapters(adapters, chain[-1]):
+                new_chain = chain + [n]
+                if new_chain[-1] == max_joltage:
+                    completed_chains.append(new_chain)
+                else:
+                    new_chains.append(new_chain)
+        chains = new_chains
+        print(len(chains))
+    return completed_chains
+
 class Test(unittest.TestCase):
-    def test_get_adapter_chain(self):
+    def test_get_complete_adapter_chain(self):
         with open('test1.txt') as f:
             adapters = parse_input(f)
         exp = [0, 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, 22]
-        self.assertEqual(get_adapter_chain(adapters), exp)
+        self.assertEqual(get_complete_adapter_chain(adapters), exp)
 
     def test_get_difference_distribution(self):
         with open('test1.txt') as f:
@@ -45,12 +66,20 @@ class Test(unittest.TestCase):
             adapters = parse_input(f)
         self.assertEqual(get_difference_distribution(adapters), Counter({1:22, 3:10}))
 
+    def test_get_all_adapter_chains(self):
+        with open('test1.txt') as f:
+            adapters = parse_input(f)
+        self.assertEqual(len(get_all_adapter_chains(adapters)), 8)
+
+        with open('test2.txt') as f:
+            adapters = parse_input(f)
+        self.assertEqual(len(get_all_adapter_chains(adapters)), 19208)
+
 if __name__ == '__main__':
     unittest.main(exit=False)
 
     with open('input.txt') as f:
         adapters = parse_input(f)
 
-    diffs = get_difference_distribution(adapters)
-    print(diffs)
-    print(diffs[1] * diffs[3])
+    chains = get_all_adapter_chains(adapters)
+    print(chains)
