b53f25d16605cce772cf88a1542418caaa81ff6c
Sam Kaplan <stkaplan@gmail.com>
2020-12-11 14:09:28 -0500

    Day 11 part 2

--- 11/11.py
+++ 11/11.py
@@ -21,26 +21,33 @@ def parse_line(line):
 def parse_input(f):
     return list(map(parse_line, f))
 
+neighbors = [
+    (-1, -1), (-1, 0), (-1, 1),
+    (0, -1), (0, 1),
+    (1, -1), (1, 0), (1, 1),
+]
+
+def is_occupied(seats, row_init, col_init, dir_row, dir_col):
+    i = 1
+    while True:
+        row = row_init + i * dir_row
+        col = col_init + i * dir_col
+        if row < 0 or col < 0:
+            return False
+        if row >= len(seats) or col >= len(seats[0]):
+            return False
+        if seats[row][col] != Seat.FLOOR:
+            return seats[row][col] == Seat.OCCUPIED
+        i += 1
+
 def get_new_seat_state(seats, row, col):
     seat = seats[row][col]
     if seat != Seat.FLOOR:
-        neighbors = [
-            (row-1, col-1), (row-1, col), (row-1, col+1),
-            (row, col-1), (row, col+1),
-            (row+1, col-1), (row+1, col), (row+1, col+1),
-        ]
-        def is_occupied(seats, r, c):
-            if r < 0 or c < 0:
-                return False
-            try:
-                return seats[r][c] == Seat.OCCUPIED
-            except IndexError:
-                return False
         if seat == Seat.EMPTY:
-            if all(not is_occupied(seats, r, c) for r, c in neighbors):
+            if all(not is_occupied(seats, row, col, dir_row, dir_col) for dir_row, dir_col in neighbors):
                 return Seat.OCCUPIED
         elif seat == Seat.OCCUPIED:
-            if sum(is_occupied(seats, r, c) for r, c in neighbors) >= 4:
+            if sum(is_occupied(seats, row, col, dir_row, dir_col) for dir_row, dir_col in neighbors) >= 5:
                 return Seat.EMPTY
     return seat
 
@@ -62,11 +69,27 @@ def count_occupied(seats):
     return sum(row.count(Seat.OCCUPIED) for row in seats)
 
 class Test(unittest.TestCase):
+    def test_is_occupied(self):
+        with open('test2.txt') as f:
+            seats = parse_input(f)
+        for dir_row, dir_col in neighbors:
+            self.assertTrue(is_occupied(seats, 4, 3, dir_row, dir_col), f'dir = {dir_row},{dir_col}')
+
+        with open('test3.txt') as f:
+            seats = parse_input(f)
+        for dir_row, dir_col in neighbors:
+            self.assertFalse(is_occupied(seats, 1, 1, dir_row, dir_col), f'dir = {dir_row},{dir_col}')
+
+        with open('test4.txt') as f:
+            seats = parse_input(f)
+        for dir_row, dir_col in neighbors:
+            self.assertFalse(is_occupied(seats, 3, 3, dir_row, dir_col), f'dir = {dir_row},{dir_col}')
+
     def test_iterate(self):
         with open('test1.txt') as f:
             seats = parse_input(f)
 
-        for i in range(1, 6):
+        for i in range(1, 7):
             seats = iterate(seats)
             with open(f'test1_{i}.txt') as f:
                 exp = parse_input(f)
@@ -80,7 +103,7 @@ class Test(unittest.TestCase):
         with open('test1.txt') as f:
             seats = parse_input(f)
         seats = converge(seats)
-        self.assertEqual(count_occupied(seats), 37)
+        self.assertEqual(count_occupied(seats), 26)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
