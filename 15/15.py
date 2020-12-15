e2154d90b9c82519abf5b23d2bcf69d36145b98e
Sam Kaplan <stkaplan@gmail.com>
2020-12-15 13:51:34 -0500

    Day 15 part 2

--- 15/15.py
+++ 15/15.py
@@ -46,8 +46,16 @@ class Test(unittest.TestCase):
         self.assertEqual(memory_game([3, 2, 1], 2020), 438)
         self.assertEqual(memory_game([3, 1, 2], 2020), 1836)
 
+        self.assertEqual(memory_game([0, 3, 6], 30000000), 175594)
+        self.assertEqual(memory_game([1, 3, 2], 30000000), 2578)
+        self.assertEqual(memory_game([2, 1, 3], 30000000), 3544142)
+        self.assertEqual(memory_game([1, 2, 3], 30000000), 261214)
+        self.assertEqual(memory_game([2, 3, 1], 30000000), 6895259)
+        self.assertEqual(memory_game([3, 2, 1], 30000000), 18)
+        self.assertEqual(memory_game([3, 1, 2], 30000000), 362)
+
 if __name__ == '__main__':
     unittest.main(exit=False)
 
     input_ = [1, 20, 8, 12, 0, 14]
-    print(memory_game(input_, 2020))
+    print(memory_game(input_, 30000000))
